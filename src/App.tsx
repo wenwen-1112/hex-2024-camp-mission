ce1739ccfe28a97ae59e6ea6aee03332dbbcf2bf
wenwen <>
2024-05-09 16:23:23 +0800

    feat: add vite+react+ts

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, neque?
+    </>
+  )
+}
+
+export default App
