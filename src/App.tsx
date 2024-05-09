0720d8177f6275ac3d8f29df4b744894f4a0f517
wenwen <>
2024-05-09 16:21:06 +0800

    feat: add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 // import { useState } from 'react'
 
 function App() {
-
-  return (
-    <>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, neque?
-    </>
-  )
+  return <>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, neque?</>
 }
 
 export default App
